d37a1fd2c196ab8c1aca10561138823333964409
psd <psd@whatfettle.com>
2014-02-19 22:01:32 +0000

    Changed pull to use FLOODDATA env

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -7,10 +7,10 @@ set -e
 
 for dir in ENT_7001 ENT_7006
 do
-	mkdir -p ~/files/$dir
-	cd ~/files/$dir
+	mkdir -p ${FLOODDATA:?}/files/$dir
+	cd ${FLOODDATA}/files/$dir
 
-	sftp -q -i $FLOODDATA_SFTP_IDENTITY -oPort=$FLOODDATA_SFTP_PORT $FLOODDATA_SFTP_PATH <<-!
+	sftp -q -i ${FLOODDATA_SFTP_IDENTITY} -oPort=${FLOODDATA_SFTP_PORT} ${FLOODDATA_SFTP_PATH} <<-!
 cd $dir
 get *
 !
