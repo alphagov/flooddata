61ac3b63b26eb529d8b874ff7564b812f120036f
psd <psd@whatfettle.com>
2014-02-19 21:48:25 +0000

    Produce daily stations files

--- scripts/stations.sh
+++ scripts/stations.sh
@@ -6,7 +6,9 @@
 set -e
 
 cd ${FLOODDATA:?}
-mkdir -p cache
+mkdir -p cache data data/stations
+export tmpfile=/tmp/stations.$$
+export header=/tmp/stations-header
 
 find files/ENT_7001 -name \*.xml |
     while read xml
@@ -14,14 +16,57 @@ find files/ENT_7001 -name \*.xml |
         tsv=cache/$xml.tsv
         if [ "$xml" -nt "$tsv" ]
         then
+            #
+            #  convert XML to TSV
+            #  - filter information deemed sensitive
+            #
             mkdir -p $(dirname "$tsv")
             (
                 set -x
                 xsltproc templates/stations.xsl $xml
-            ) | grep -v 'Gate Position' > $tsv
+            ) |
+            grep -v 'Gate Position' > $tsv
+
+            #
+            #  capture header
+            #
+            head -1 $tsv > $header
+
+            #
+            #  add to daily file
+            #
+            cat $tsv |
+            awk -F'	' '
+               $1 ~ /^20[0-9][0-9]-[0-9][0-9]-[0-9][0-9]/ {
+                    day=$1;
+                    sub(/T.*$/,"",day);
+                    gsub(/-/,"",day);
+                    file="data/stations/" day ".tsv";
+                    print >> file;
+                    print file
+               }'
         fi
+    done |
+
+    #
+    #  de-dupe updated daily files
+    #
+    sort -u |
+    while read file
+    do
+        echo "updating $file .." >&2
+        (
+            cat $header
+            cat $file
+        ) |
+        sort -n |
+        uniq > $tmpfile
+        mv $tmpfile $file
     done
 
+#
+#  generate latest file
+#
 find cache/files/ENT_7001 -name \*.tsv |
     xargs cat |
     sort -n |
@@ -32,4 +77,3 @@ find cache/files/ENT_7001 -name \*.tsv |
 #  - for backwards compatibility with big file made for the hackday
 #
 gzip -c < data/stations.tsv > data/stations.tsv.gz
-
