e55365220fbbd6ca58c581f5bdf1eb8773c41be0
psd <psd@whatfettle.com>
2014-02-19 22:10:07 +0000

    Unzip updated, not all files

--- scripts/unzip.sh
+++ scripts/unzip.sh
@@ -3,11 +3,11 @@
 #
 #  unzip files
 #
-cd ~/files/ENT_7001
+cd ${FLOODDATA:?}/files/ENT_7001
 
 for zip in *.zip
 do
 	dir=$(basename $zip .zip)
 	mkdir -p $dir
-	unzip -q -LL -o $zip -d $dir
+	unzip -q -LL -u -o $zip -d $dir
 done
